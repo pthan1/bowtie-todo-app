b76674f35c5af5f54e2ae4e931c0ea6696d09970
Phil Than <82873669+pthan1@users.noreply.github.com>
2022-05-01 16:20:16 -0600

    Install Typescript

---
+++ src/components/App.tsx
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+const App = () => {
+  const [projects, setProjects] = useState<Project[]>([]);
+  return <div className="App">Hello</div>;
+};
+
+export default App;
