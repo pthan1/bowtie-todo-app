28e193200467d01423509afe7818e0893ca6ab9a
Phil Than <82873669+pthan1@users.noreply.github.com>
2022-05-02 02:12:31 -0600

    Add New Project form

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,13 @@
 import { EntityId, Project as APIProject } from "../types/.d";
-import React, { useEffect, useState } from "react";
+import React, { FormEventHandler, useEffect, useState } from "react";
 
 import "./App.css";
 import Project from "./Project";
 
 const App = () => {
   const [projects, setProjects] = useState<APIProject[]>([]);
-  const [newProjectName, setNewProjectName] = useState("");
+  const [projectNameEntryText, setProjectNameEntryText] = useState("");
+  const [changedProjectIds, setChangedProjectIds] = useState<EntityId[]>([]);
 
   useEffect(() => {
     fetch("http://localhost:3001/api/v1/projects")
@@ -22,14 +23,38 @@ const App = () => {
         }
       })
       .catch((error) => console.log(error));
-  }, []);
+  }, [changedProjectIds]);
+
+  const onSubmit: FormEventHandler = (e) => {
+    e.preventDefault();
+    fetch("http://localhost:3001/api/v1/projects", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        name: projectNameEntryText,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => setChangedProjectIds(data))
+      .catch((error) => console.log(error));
+  };
+
   return (
     <div className="App">
       <>
         {projects.length === 0 && "There are no projects."}
-        {projects.map(({ id, name, tasks }) => {
-          <Project id={id} name={name} tasks={tasks} />;
-        })}
+        <form onSubmit={onSubmit}>
+          <label htmlFor="name">New Project Name</label>
+          <input
+            type="text"
+            id="New Project Name"
+            value={projectNameEntryText}
+            onChange={(e) => setProjectNameEntryText(e.target.value)}
+          />
+        </form>
+        {projects.map(({ id, name, tasks }) => (
+          <Project id={id} name={name} tasks={tasks} />
+        ))}
       </>
     </div>
   );
